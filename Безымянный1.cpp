bff13b11c38cffa2ed1cc4ae4a7e953480b87c53
eugenepominov <47748680+eugenepominov@users.noreply.github.com>
2019-02-26 16:28:06 +0700

    Add files via upload

---
+++ Безымянный1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char** argv) {
+ 
+int a, b, c;
+ 
+cout << "Input first number" << endl;
+cin >> a;
+ 
+cout << "Input second number" << endl;
+cin >> b;
+ 
+c = a + b;
+ 
+cout << "Sum " << c << endl;
+ 
+return 0;
+}
+//Anastasia Iscrizhickaya
